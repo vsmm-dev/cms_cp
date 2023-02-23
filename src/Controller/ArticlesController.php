228aa00bb6cb9e5e9e44aebbb70bd70bbccf36f5
vsmm-dev <104638472+vsmm-dev@users.noreply.github.com>
2023-02-23 09:04:30 -0600

    Initial commit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,42 @@
+<?php
+// src/Controller/ArticlesController.php
+namespace App\Controller;
+
+class ArticlesController extends AppController
+{
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash'); // Include the FlashComponent
+    }
+
+    public function index()
+    {
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
+
+    public function view($slug)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
+
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            // Hardcoding the user_id is temporary, and will be removed later
+            // when we build authentication out.
+            $article->user_id = 1;
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
+    }
+}
